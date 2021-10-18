b0d3843085bbb1787ad2ca5ee2d2d33b8331fa44
Francesco Dente <dente.francesco1996@gmail.com>
2021-10-18 10:08:50 +0200

    Add empty data access installer

---
+++ src/___MICROSERVICE_NAME___.Web/DependencyInjection/DataAccessInstaller.cs
@@ -0,0 +1,18 @@
+﻿using EasyDesk.CleanArchitecture.Dal.EfCore;
+using EasyDesk.CleanArchitecture.Web.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ___MICROSERVICE_NAME___.Web.DependencyInjection
+{
+    public class DataAccessInstaller : IServiceInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            services.AddEfCoreDataAccess(configuration.GetConnectionString("MainDb"), options =>
+            {
+            });
+        }
+    }
+}
